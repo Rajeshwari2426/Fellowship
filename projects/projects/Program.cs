8b378fc6d34feb0ddb6540d77ab59706a1817bc8
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-01-17 17:08:35 +0530

    [Rajeshwari] adding fibonocci series program

--- projects/projects/Program.cs
+++ projects/projects/Program.cs
@@ -10,6 +10,8 @@ namespace projects
     {
         static void Main(string[] args)
         {
+            FibonocciSeries series = new FibonocciSeries();
+            series.FindFibonocciSeries();
         }
     }
 }
