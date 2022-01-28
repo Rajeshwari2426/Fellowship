754b8ab5ff9fd256d7a811e3b8f06a723b1f8ad9
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-01-28 17:36:12 +0530

    adding Quotient And Remainder

--- projects/projects/Program.cs
+++ projects/projects/Program.cs
@@ -10,19 +10,20 @@ namespace projects
     {
         static void Main(string[] args)
         {
-            /*  FibonocciSeries series = new FibonocciSeries();
-               series.FindFibonocciSeries();
-               ReverseNumbers.FindReverseNumber();
-              Console.ReadLine();
-              SwapNumbers.SwappingNumbers();
-              CoinFlip flipCoin = new CoinFlip();
-              flipCoin.FlipCoin();
-              LeapYear leapYear  = new LeapYear();
-              leapYear.LeapYearCheck();
+            /*   FibonocciSeries series = new FibonocciSeries();
+                series.FindFibonocciSeries();
+                ReverseNumbers.FindReverseNumber();
+               Console.ReadLine();
+               SwapNumbers.SwappingNumbers();
+               CoinFlip flipCoin = new CoinFlip();
+               flipCoin.FlipCoin();
+               LeapYear leapYear  = new LeapYear();
+               leapYear.LeapYearCheck();
 
-             PowerOfTwo.PrintTable();*/
-            // HarmonicNumber.GetHarmonic();
-            Factors.GetPrimeFactors();
+              PowerOfTwo.PrintTable();
+              HarmonicNumber.GetHarmonic();
+             Factors.GetPrimeFactors();*/
+            QuotientAndRemainder.GetQuotientRemainder();
             Console.ReadLine();
         }
     }
---
+++ projects/projects/QuotientAndRemainder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projects
+{
+    internal class QuotientAndRemainder
+    {
+        public static void GetQuotientRemainder()
+        {
+            Console.WriteLine("Enter the Dividend: ");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+        }
+    }
+}
