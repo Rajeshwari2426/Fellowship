aedb1de1bfc6092f8fec351f73464488a3e943a7
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-02-05 10:56:28 +0530

    adding Perfect number

--- projects/projects/Program.cs
+++ projects/projects/Program.cs
@@ -9,25 +9,41 @@ namespace projects
     internal class Program
     {
         static void Main(string[] args)
-        {
-            /*   FibonocciSeries series = new FibonocciSeries();
-                series.FindFibonocciSeries();
-                ReverseNumbers.FindReverseNumber();
+        {    
+           /* //fibonocciseries ........
+               FibonocciSeries series = new FibonocciSeries();
+               series.FindFibonocciSeries();
+
+            //Reverse numbers ......
+               ReverseNumbers.FindReverseNumber();
                Console.ReadLine();
+
+            //swapping of two numbers
                SwapNumbers.SwappingNumbers();
+            //flipping of coins
                CoinFlip flipCoin = new CoinFlip();
                flipCoin.FlipCoin();
+            //check given year is leap year or not
                LeapYear leapYear  = new LeapYear();
                leapYear.LeapYearCheck();
-
+            //print powers of 2
               PowerOfTwo.PrintTable();
+            //find value of nth harmonic
               HarmonicNumber.GetHarmonic();
-             Factors.GetPrimeFactors();*/
-            // QuotientAndRemainder.GetQuotientRemainder();
-            // EvenOdd.EvenOddCheck();
-          //  VowelConsonant.VowelCheck();
-          LargestNumber largest=new LargestNumber();
-            largest.GetLargest();
+            //get prime factors of given number
+             Factors.GetPrimeFactors();
+            //find quotient and remainder of given number
+             QuotientAndRemainder.GetQuotientRemainder();
+            //check number is even or odd
+             EvenOdd.EvenOddCheck();
+            //check given letter is vowel or consonant
+             VowelConsonant.VowelCheck();
+            //find largest of three numbers
+            LargestNumber largest=new LargestNumber();
+            largest.GetLargest();*/
+            //finding perfect number
+            PerfectNumber perfectnum = new PerfectNumber();
+            perfectnum.Perfect();
             Console.ReadLine();
         }
     }
